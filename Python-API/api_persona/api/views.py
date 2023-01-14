a05ccff0dd54f5debdec5c1e4aaa9a30f9f8a60b
dspvillarroel <77422030+dspvillarroel@users.noreply.github.com>
2023-01-13 21:35:57 -0500

    Add files via upload

---
+++ Python-API/api_persona/api/views.py
@@ -0,0 +1,15 @@
+from rest_framework import viewsets
+from api.models import Persona,Tipo_persona
+from api.serializers import PersonaSerializer,TipoPersonaSerializer
+
+class TipoPersonaViewSet ( viewsets.ModelViewSet ):
+    serializer_class = TipoPersonaSerializer
+    queryset = Tipo_persona.objects.all()
+   
+class PersonaViewSet ( viewsets.ModelViewSet ):
+    serializer_class = PersonaSerializer
+    queryset = Persona.objects.all()
+
+#... otras viewset
+
+# Create your views here.
